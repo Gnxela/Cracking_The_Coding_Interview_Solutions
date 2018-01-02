6221c3d87edbf19b147ea5b0094137f54031c140
Gnxela <qawswedfrgtt@gmail.com>
2018-01-02 15:07:56 +0000

    Updated question 4. Now answers the question fully.

--- src/me/alexng/crackingTheCodeInterview/q4_3.java
+++ src/me/alexng/crackingTheCodeInterview/q4_3.java
@@ -3,6 +3,8 @@ package me.alexng.crackingTheCodeInterview;
 import me.alexng.crackingTheCodeInterview.util.BinaryNode;
 import me.alexng.crackingTheCodeInterview.util.SNode;
 
+import java.util.HashMap;
+
 public class q4_3 {
 
     public static void main(String[] args) {
@@ -14,8 +16,9 @@ public class q4_3 {
         n.right.left = new BinaryNode(821);
         n.right.right = new BinaryNode(822);
         n.right.right.right = new BinaryNode(8222);
+        n.left.left.left = new BinaryNode(8111);
 
-        SNode<Integer> level = getLevel(n, 3);
+        SNode<Integer> level = getLevels(n, 3).get(3);
         while (level != null) {
             System.out.println(level.data);
             level = level.next;
@@ -45,6 +48,43 @@ public class q4_3 {
         }
 
         return node;
+    }
+
+    public static HashMap<Integer, SNode<Integer>> getLevels(BinaryNode<Integer> base, int level) {
+        HashMap<Integer, SNode<Integer>> map = new HashMap<>();
+        map.put(0, new SNode<>(base.data));
+        return getLevels(base, map, level, 0);
+    }
+
+    private static HashMap<Integer, SNode<Integer>> getLevels(BinaryNode<Integer> base, HashMap<Integer, SNode<Integer>> map, int level, int running) {
+        if (base == null) {
+            return null;
+        }
+
+        SNode node = map.get(++running);
+        if (node == null) {//Logic is a bit ugly but works
+            if (base.left != null) {
+                node = new SNode(base.left.data);
+                if (base.right != null) {
+                    node.appendToTail(base.right.data);
+                }
+            } else if (base.right != null) {
+                node = new SNode(base.right.data);
+            }
+            map.put(running, node);
+        } else {
+            if (base.left != null)
+                node.appendToTail(base.left.data);
+            if (base.right != null)
+                node.appendToTail(base.right.data);
+        }
+
+        if (level != running) {
+            getLevels(base.left, map, level, running);
+            getLevels(base.right, map, level, running);
+        }
+
+        return map;
     }
 
 }
